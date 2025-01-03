e647912e307caef353b245aff1cd655a46f1433c
Guilherme Martins <guilherme.g.martins@gmail.com>
2025-01-02 21:27:24 -0300

    Raposinha 123

--- js-curso-2-aula_4/app.js
+++ js-curso-2-aula_4/app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Raposinha 1 2 3 o loko meu ');
 }
 
 exibirMensagemInicial();
